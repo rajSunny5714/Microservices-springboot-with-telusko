42d422956a85ec732f0861812d6aa7e843559cb4
sunnyraj <mrsj5714@gmail.com>
2026-05-03 20:18:25 +0530

    quizapp project updated

--- quizapp/src/main/java/com/telusko/quizapp/service/QuestionService.java
+++ quizapp/src/main/java/com/telusko/quizapp/service/QuestionService.java
@@ -1,13 +1,17 @@
 package com.telusko.quizapp.service;
 
 import com.telusko.quizapp.Question;
+import com.telusko.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
     public List<Question> getAllQuestions() {
-
+        return questionDao.findAll();
     }
 }
