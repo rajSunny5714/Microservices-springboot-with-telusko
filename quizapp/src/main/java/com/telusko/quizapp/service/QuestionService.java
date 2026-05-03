69333937bc623dc0069aea97ca90aa43f15b2c1a
sunnyraj <mrsj5714@gmail.com>
2026-05-03 19:49:52 +0530

    repo name updated

---
+++ quizapp/src/main/java/com/telusko/quizapp/service/QuestionService.java
@@ -0,0 +1,13 @@
+package com.telusko.quizapp.service;
+
+import com.telusko.quizapp.Question;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    public List<Question> getAllQuestions() {
+
+    }
+}
