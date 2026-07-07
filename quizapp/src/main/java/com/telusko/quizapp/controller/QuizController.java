a237580e72a07b858a06e52bd0afb6e2b69d0d71
sunnyraj <mrsj5714@gmail.com>
2026-07-07 20:11:19 +0530

    update quizapp file code

---
+++ quizapp/src/main/java/com/telusko/quizapp/controller/QuizController.java
@@ -0,0 +1,14 @@
+package com.telusko.quizapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
+        return new ResponseEntity<>("I'm here", HttpStatus.OK);
+    }
+}
